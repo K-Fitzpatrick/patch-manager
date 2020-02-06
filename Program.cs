d3b3e4cf75cf8bea9ad7048fa956fa8dc637b0f5
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2020-02-06 03:48:50 -0600

    Download patches from URLs, implement patches.lock, and allow for using the locked patches as the absolute versions
    The locked patches will be used if there's no location or url given, or if `update` is `false`.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using CodeIsle.LibIpsNet;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
 using System.Web.Script.Serialization;
 
 namespace ips_patch_manager
@@ -10,12 +12,14 @@ namespace ips_patch_manager
         public string name;
         public string url;
         public string location;
+        public bool? update;
     }
 
     public class PatchFile
     {
         public string baseRomLocation;
         public string outputRomLocation;
+        public string patchfileLockDirectory;
         public List<Patch> patches;
     }
 
@@ -27,18 +31,102 @@ namespace ips_patch_manager
             var serializer = new JavaScriptSerializer();
             PatchFile patchFile = serializer.Deserialize<PatchFile>(text);
 
-            // Use URL to download patches into a temporary file, updating the location in-memory
-            // Then apply the patches in the described order
+            List<string> patchPaths = LockAllPatches(patchFile);
 
-            Patch(patchFile);
+            Patch(patchPaths, patchFile.baseRomLocation, patchFile.outputRomLocation);
         }
 
-        static void Patch(PatchFile patchFile)
+        static List<string> LockAllPatches(PatchFile patchFile)
+        {
+            List<string> patchPaths = new List<string>();
+            string tempDirectoryPath = Path.Combine(Path.GetTempPath(), "patches.lock");
+
+            try
+            {
+                if(Directory.Exists(tempDirectoryPath))
+                    Directory.Delete(tempDirectoryPath, true);
+                var tempDirectory = Directory.CreateDirectory(tempDirectoryPath);
+
+                var lockDirectory = Directory.CreateDirectory(patchFile.patchfileLockDirectory);
+                string lockDirectoryPath = lockDirectory.FullName;
+
+                if(patchFile.patches.Any(patch => patch.name == null))
+                {
+                    throw new System.Exception("No name found on patch");
+                }
+
+                if(patchFile.patches.GroupBy(patch => patch.name).Where(group => group.Count() > 1).Any())
+                {
+                    throw new System.Exception("Duplicate names found");
+                }
+
+                foreach(var patch in patchFile.patches)
+                {
+                    string lockPatchLocation = Path.Combine(lockDirectoryPath, $"{patch.name}.ips");
+                    string tempPatchLocation = Path.Combine(tempDirectoryPath, $"{patch.name}.ips");
+
+                    if(patch.update ?? true)
+                    {
+                        if(patch.location != null)
+                        {
+                            File.Copy(patch.location, tempPatchLocation);
+                        }
+                        else if(patch.url != null)
+                        {
+                            using(var client = new WebClient())
+                            {
+                                client.DownloadFile(patch.url, tempPatchLocation);
+                            }
+                        }
+                        else if(File.Exists(lockPatchLocation))
+                        {
+                            File.Copy(lockPatchLocation, tempPatchLocation);
+                        }
+                        else if(!File.Exists(lockPatchLocation))
+                        {
+                            throw new System.Exception($"No Location or URL specified for '{patch.name}', and patch does not already exist in lock directory");
+                        }
+                    }
+                    else
+                    {
+                        if(File.Exists(lockPatchLocation))
+                        {
+                            File.Copy(lockPatchLocation, tempPatchLocation);
+                        }
+                        else if(!File.Exists(lockPatchLocation))
+                        {
+                            throw new System.Exception($"Patch '{patch.name}' does not already exist in lock directory, and `update` is `false`");
+                        }
+                    }
+
+                    patchPaths.Add(lockPatchLocation);
+                }
+
+                if(Directory.Exists(patchFile.patchfileLockDirectory))
+                    Directory.Delete(patchFile.patchfileLockDirectory, true);
+                lockDirectory = Directory.CreateDirectory(patchFile.patchfileLockDirectory);
+
+                foreach(FileInfo file in tempDirectory.GetFiles())
+                {
+                    File.Move(file.FullName, Path.Combine(lockDirectoryPath, file.Name));
+                }
+
+                Directory.Delete(tempDirectoryPath, true);
+            }
+            catch
+            {
+                Directory.Delete(tempDirectoryPath, true);
+                throw;
+            }
+            return patchPaths;
+        }
+
+        static void Patch(List<string> patchPaths, string baseRomLocation, string outputRomLocation)
         {
             Patcher patcher = new Patcher();
             using(MemoryStream targetStream = new MemoryStream())
             {
-                using(FileStream sourceStream = File.Open(patchFile.baseRomLocation, FileMode.Open, FileAccess.Read))
+                using(FileStream sourceStream = File.Open(baseRomLocation, FileMode.Open, FileAccess.Read))
                 {
                     sourceStream.CopyTo(targetStream);
                 }
@@ -46,16 +134,16 @@ namespace ips_patch_manager
                 // empty stream, so targetStream just keeps being reused over and over again
                 using(MemoryStream emptyStream = new MemoryStream())
                 {
-                    foreach(Patch patch in patchFile.patches)
+                    foreach(string patchPath in patchPaths)
                     {
-                        using(FileStream patchStream = File.Open(patch.location, FileMode.Open, FileAccess.Read))
+                        using(FileStream patchStream = File.Open(patchPath, FileMode.Open, FileAccess.Read))
                         {
                             patcher.Patch(patchStream, emptyStream, targetStream);
                         }
                     }
                 }
 
-                File.WriteAllBytes(patchFile.outputRomLocation, targetStream.ToArray());
+                File.WriteAllBytes(outputRomLocation, targetStream.ToArray());
             }
         }
     }
