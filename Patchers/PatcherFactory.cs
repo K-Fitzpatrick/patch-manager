b59cbe4933c65e16d230cb9ab1228de274ad8947
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2020-02-08 14:33:08 -0600

    WIP - Create Patcher abstractions

---
+++ Patchers/PatcherFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ips_patch_manager.Patchers
+{
+    public static class PatcherFactory
+    {
+        public IPatcher CreatePatcher()
+        {
+
+        }
+    }
+}
