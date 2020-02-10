47a56a573056b1f5d7c59b50c5c758cad8cf19d1
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2020-02-09 19:43:12 -0600

    Write tests for IPS using known files

---
+++ patch-applier-tests/PatchApplierTests/PatchTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using patch_applier.PatchFileDescription;
+using System.Collections.Generic;
+using patch_applier;
+using System.Linq;
+using System.IO;
+using ips_patch_manager.Patchers;
+
+namespace patch_applier_tests.PatchApplierTests
+{
+    [TestClass]
+    public class PatchTests
+    {
+        readonly string CHANGE_1 = "1.IPS";
+        readonly string CHANGE_2 = "2.IPS";
+        readonly string CHANGE_3 = "3.IPS";
+
+        readonly string TEST_PATCH_DIR = "TestPatches";
+        readonly string BASE_FILE = "base-file.smc";
+
+        Stream RunPatches(List<string> patchPaths)
+        {
+            string baseFilePath = Path.Combine(TEST_PATCH_DIR, BASE_FILE);
+            var patchers = patchPaths.Select(path => PatcherFactory.CreatePatcher(path)).ToList();
+
+            using(Stream baseFile = new FileStream(baseFilePath, FileMode.Open, FileAccess.Read))
+            {
+                Stream target = new MemoryStream();
+                PatchApplier.Patch(patchers, baseFile, target);
+                return target;
+            }
+        }
+
+        List<string> GetPatchPaths(List<string> names)
+        {
+            return names
+                .Select(name => Path.Combine(TEST_PATCH_DIR, name))
+                .ToList();
+        }
+
+        FileStream OpenFile(string fileName)
+        {
+            return new FileStream(Path.Combine(TEST_PATCH_DIR, fileName), FileMode.Open, FileAccess.Read);
+        }
+
+        bool IsEqual(Stream left, Stream right)
+        {
+            if(left.Length != right.Length)
+                return false;
+
+            left.Position = 0;
+            right.Position = 0;
+
+            var leftBytes = new byte[left.Length];
+            left.Read(leftBytes, 0, (int)left.Length);
+            var rightBytes = new byte[right.Length];
+            right.Read(rightBytes, 0, (int)right.Length);
+
+            return leftBytes.SequenceEqual(rightBytes);
+        }
+
+        [TestMethod]
+        public void NoPatch()
+        {
+            var patches = GetPatchPaths(new List<string>
+            {
+            });
+
+            using(Stream expectedResult = OpenFile(BASE_FILE))
+            using(Stream actual = RunPatches(patches))
+            {
+                Assert.IsTrue(IsEqual(expectedResult, actual), "Streams do not match!");
+            }
+        }
+
+        [TestMethod]
+        public void Patch1()
+        {
+            var patches = GetPatchPaths(new List<string>
+            {
+                CHANGE_1,
+            });
+
+            using(Stream expectedResult = OpenFile("base-file-1.smc"))
+            using(Stream actual = RunPatches(patches))
+            {
+                Assert.IsTrue(IsEqual(expectedResult, actual), "Streams do not match!");
+            }
+        }
+
+        [TestMethod]
+        public void Patch12()
+        {
+            var patches = GetPatchPaths(new List<string>
+            {
+                CHANGE_1,
+                CHANGE_2,
+            });
+
+            using(Stream expectedResult = OpenFile("base-file-1-2.smc"))
+            using(Stream actual = RunPatches(patches))
+            {
+                Assert.IsTrue(IsEqual(expectedResult, actual), "Streams do not match");
+            }
+        }
+
+
+        [TestMethod]
+        public void PatchingOutOfOrderFails()
+        {
+            var patches = GetPatchPaths(new List<string>
+            {
+                CHANGE_2,
+                CHANGE_1,
+            });
+
+            using(Stream expectedResult = OpenFile("base-file-1-2.smc"))
+            using(Stream actual = RunPatches(patches))
+            {
+                Assert.IsFalse(IsEqual(expectedResult, actual), "Streams match, but they shouldn't");
+            }
+        }
+    }
+}
---
+++ patch-applier-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("patch-applier-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("patch-applier-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("55f66c43-3bff-4b22-9978-3461fb896118")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
