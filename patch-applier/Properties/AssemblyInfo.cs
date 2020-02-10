c32f3463b726172d00efd678f6998be20a726f97
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2020-02-09 22:29:05 -0600

    Add .nuspec file to patch-applier
    This is the project that will be used by other projects.
    I'll probably want move it to a completely separate project eventually.

--- patch-applier/Properties/AssemblyInfo.cs
+++ patch-applier/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("patch-applier")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Run IPS and ASM patches in a specified order")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kyle Fitzpatrick")]
 [assembly: AssemblyProduct("patch-applier")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
