1b452eda965c34e058376b217c45bc071cd3c187
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2020-02-18 23:27:22 -0600

    Add winforms UI project for patch applier
    Quick 'n dirty

---
+++ patch-applier-gui/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace patch_applier_gui
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PatchManagerForm());
+        }
+    }
+}
