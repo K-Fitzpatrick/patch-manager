45f1135125bd5822ba4c17cf341a2de6b42cf4fa
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2020-02-20 21:26:33 -0600

    Create ROM based on a user-selected patchfile.json

--- patch-applier-gui/PatchManagerForm.Designer.cs
+++ patch-applier-gui/PatchManagerForm.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.bOpenFileDialog = new System.Windows.Forms.Button();
+            this.tbPatchFilePath = new System.Windows.Forms.TextBox();
+            this.bPatch = new System.Windows.Forms.Button();
+            this.folderBrowserDialogPatchFile = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialogPatchFile = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
+            // 
+            // bOpenFileDialog
+            // 
+            this.bOpenFileDialog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bOpenFileDialog.Location = new System.Drawing.Point(134, 44);
+            this.bOpenFileDialog.Name = "bOpenFileDialog";
+            this.bOpenFileDialog.Size = new System.Drawing.Size(75, 23);
+            this.bOpenFileDialog.TabIndex = 0;
+            this.bOpenFileDialog.Text = "Browse...";
+            this.bOpenFileDialog.UseVisualStyleBackColor = true;
+            this.bOpenFileDialog.Click += new System.EventHandler(this.bOpenFileDialog_Click);
+            // 
+            // tbPatchFilePath
+            // 
+            this.tbPatchFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPatchFilePath.Location = new System.Drawing.Point(12, 12);
+            this.tbPatchFilePath.Name = "tbPatchFilePath";
+            this.tbPatchFilePath.Size = new System.Drawing.Size(278, 22);
+            this.tbPatchFilePath.TabIndex = 1;
+            this.tbPatchFilePath.Text = "patchfile.json";
+            // 
+            // bPatch
+            // 
+            this.bPatch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bPatch.Location = new System.Drawing.Point(215, 44);
+            this.bPatch.Name = "bPatch";
+            this.bPatch.Size = new System.Drawing.Size(75, 23);
+            this.bPatch.TabIndex = 2;
+            this.bPatch.Text = "Patch";
+            this.bPatch.UseVisualStyleBackColor = true;
+            this.bPatch.Click += new System.EventHandler(this.bPatch_Click);
+            // 
+            // openFileDialogPatchFile
+            // 
+            this.openFileDialogPatchFile.FileName = "openFileDialogPatchFile";
             // 
             // PatchManagerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(302, 78);
+            this.Controls.Add(this.bPatch);
+            this.Controls.Add(this.tbPatchFilePath);
+            this.Controls.Add(this.bOpenFileDialog);
+            this.MinimumSize = new System.Drawing.Size(320, 125);
             this.Name = "PatchManagerForm";
-            this.Text = "Form1";
+            this.Text = "Apply Patches";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bOpenFileDialog;
+        private System.Windows.Forms.TextBox tbPatchFilePath;
+        private System.Windows.Forms.Button bPatch;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogPatchFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialogPatchFile;
     }
 }
 
--- patch-applier-gui/PatchManagerForm.cs
+++ patch-applier-gui/PatchManagerForm.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using patch_applier;
+using patch_applier.PatchFileDescription;
+using patch_manager.Patchers;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace patch_applier_gui
@@ -15,6 +16,65 @@ namespace patch_applier_gui
         public PatchManagerForm()
         {
             InitializeComponent();
+        }
+
+        private void bOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialogPatchFile.ShowDialog();
+
+            if(result == DialogResult.OK
+                && !String.IsNullOrWhiteSpace(openFileDialogPatchFile.FileName))
+            {
+                tbPatchFilePath.Text = openFileDialogPatchFile.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Patches according to the selected patchfile.
+        /// All paths should be relative to the selected file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bPatch_Click(object sender, EventArgs e)
+        {
+            string patchFilePath = tbPatchFilePath.Text;
+
+            if(String.IsNullOrWhiteSpace(patchFilePath))
+            {
+                MessageBox.Show("Please select a file");
+                return;
+            }
+
+            if(!File.Exists(patchFilePath))
+            {
+                MessageBox.Show("File not found");
+                return;
+            }
+
+            try
+            {
+                CreateRom(patchFilePath);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Unexpected error");
+                return;
+            }
+        }
+
+        private void CreateRom(string patchFilePath)
+        {
+            string text = File.ReadAllText(patchFilePath);
+            var serializer = new JavaScriptSerializer();
+            PatchFile patchFile = serializer.Deserialize<PatchFile>(text);
+
+            string directory = Path.GetDirectoryName(patchFilePath);
+            Directory.SetCurrentDirectory(directory);
+
+            List<string> patchPaths = PatchApplier.LockAllPatches(patchFile.patches, patchFile.patchfileLockDirectory);
+
+            var patchers = patchPaths.Select(path => PatcherFactory.CreatePatcher(path)).ToList();
+            PatchApplier.Patch(patchers, patchFile.baseRomLocation, patchFile.outputRomLocation);
         }
     }
 }
